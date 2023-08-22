34325c071d80054c5876ad1b4f98f9e359991a9b
SProkopios <s.koponen93@gmail.com>
2023-08-22 19:30:20 +0300

    Tehtävä 1 (Laskin)

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
-const jotain = () => {
-  Alert.alert("Toimii")
-};
+
+
+
 
 export default function App() {
+  const [numberone, setNumberone] = useState();
+  const [numbertwo, setNumbertwo] = useState();
+  const [answer, setAnswer] = useState();
+
+  const add = () => {
+    setAnswer(parseInt(numberone) + parseInt(numbertwo));
+  }
+
+  const unadd = () => {
+    setAnswer(parseInt(numberone) - parseInt(numbertwo));
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Button onPress={jotain} title='Toimiiko?' />
-      <Text>TESTI 123</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Result: {answer}</Text>
+      <View>
+        <TextInput keyboardType='numeric' placeholder='Number' style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={numberone => setNumberone(numberone)} value={numberone} />
+        <TextInput keyboardType='numeric' placeholder='Number' style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={numbertwo => setNumbertwo(numbertwo)} value={numbertwo} />
+
+      </View>
+      <View style={{flexDirection: 'row', alignItems: 'flex-start'}}>
+        <Button onPress={add} title="+" />
+        <Button onPress={unadd} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
