094b04c2cf04bd2b5cda83868385df64b6731bc7
SProkopios <s.koponen93@gmail.com>
2023-09-18 12:46:28 +0300

    Tehätävä 8

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,48 +1,35 @@
 
 import React, { useEffect, useState } from 'react';
-import { Text, View, Button, TextInput, Alert } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import {View} from 'react-native';
 
 
-//Tämä näytti toimivan ilman Object.keys() 
+
+ 
 export default function HomeScreen() {
-    const [amount, setAmount] = useState('');
-    const [rate, setRate] = useState([]);
-    const [currency, setCurrency] = useState('');
-    const API_KEY = "404";
 
-    const requestData = {
-      method: 'GET',
-      redirect: 'follow',
-      headers: {apikey: API_KEY}
-    }
-
-    const getRate = () => {
-      fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${currency}&amount=${amount}`, requestData)
-      .then(response => response.json())
-      .then(data => setRate(data.result))
-      .catch(error => {
-        Alert.alert('Error', error);
-        });
-      }
 
       
 
     return (
-      <View style={{marginTop: 20, marginBottom: 60, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>{rate} €</Text>
-        <View style={{justifyContent: 'center',flexDirection: 'row'}}>
-          <TextInput style={{width: 60}} keyboardType='numeric' value={amount} onChangeText={amount => setAmount(amount)}/>
-          <Picker style={{width: 120}} selectedValue={currency} onValueChange={(itemValue, itemIndex) =>
-            setCurrency(itemValue)
-          }>
-            <Picker.Item label="JPY" value="JPY" />
-            <Picker.Item label="ARS" value="ARS" />
-            <Picker.Item label="TRY" value="TRY" />
-            <Picker.Item label="USD" value="USD" />
-          </Picker>
-        </View>
-        <Button title="Convert" onPress= {getRate} />
+      <View>
+        <MapView 
+          style={{
+            flex: 1,
+            height: '100%',
+            width: '100%'
+          }}
+          initialRegion={initial}
+          region={initial}
+          >
+            <Marker 
+              coordinate={{
+                latitude: 60.200692,
+                longitude: 24.934302
+              }}
+              title="Haaga-Helia"
+              description="ammattikorkeakoulu"
+           />
+          </MapView>
       </View>
       );
     }
