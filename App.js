61f7016e02007e3db21b24818131d95b4cfb6ba5
SProkopios <s.koponen93@gmail.com>
2023-09-27 21:28:27 +0300

    Tehtävä 11

--- App.js
+++ App.js
@@ -1,79 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, StatusBar, View, TextInput, Button } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+import { View, FlatList, TextInput, Button, StyleSheet, Text } from "react-native";
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from "react";
+
 
 export default function App() {
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
+  const [shoppinglist, setShoppinglist] = useState([]);
+  const db = SQLite.openDatabase('shoppinglistdb.db');
 
-  const [address, setAdress] = useState('');
-  const [longitude, setLongitude] = useState(24.934302);
-  const [latitude, setLatitude] = useState(60.200692);
-  const API_KEY = "404";
-  
 
-  useEffect(() => (async() => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert('No permission to get location')
-      return;
-    }
-
-    let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
-    setLatitude(location.coords.latitude);
-    setLongitude(location.coords.longitude)
-    console.log('Location:', location.coords.latitude)
-  })(), []);
-
-  const getPlace = () => {
-    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${API_KEY}&location=${address}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log("LATITUDE:" + data.results[0].locations[0].latLng.lat)
-      console.log("LONGITUDE:" + data.results[0].locations[0].latLng.lng)
-      console.log("current latitude: " + latitude)
-      setLatitude(data.results[0].locations[0].latLng.lat),
-      setLongitude(data.results[0].locations[0].latLng.lng)
-    })
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppinglist (id integer primary key not null, amount text, product text);');
+    }, () => console.error("Error when creating DB"), updateList);
+  }, []);
+    
+    
+  const saveItem = () => {
+    db.transaction(tx => {
+      tx.executeSql('insert into shoppinglist (amount, product) values (?, ?);',
+        [amount, product]);
+      }, null, updateList)
   }
-  const coordinates = {
-    latitude: latitude,
-    longitude: longitude
-  };
+    
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppinglist;', [], (_, { rows }) =>
+        setShoppinglist(rows._array)
+      );
+    }, null, null);
+  }
+    
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => tx.executeSql('delete from shoppinglist where id = ?;', [id]), null, updateList);
+  }
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.0322,
-          longitudeDelta: 0.0221
-        }}
-      >
-        <Marker
-          coordinate={coordinates}
-          title='HERE'
-        />
-      </MapView>
-      <TextInput value={address} onChangeText={address => setAdress(address)} />
-      <Button onPress={getPlace} title='FIND IT' />
+      <TextInput
+        style={styles.texts}
+        placeholder='Product'
+        onChangeText={product => setProduct(product)}
+        value={product}/>
+      <TextInput
+        style={styles.texts}
+        placeholder='Amount'
+        onChangeText={amount => setAmount(amount)}
+        value={amount}/>
+      <Button onPress={saveItem} title="Save" />
+      <Text>Shopping list</Text>
+      <FlatList
+        style={{marginLeft : "5%"}}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) =>
+        <View style={styles.listcontainer}>
+          <Text>{item.product}, {item.amount} </Text>
+          <Text style={{color: '#0000ff'}} onPress={() => deleteItem(item.id)}>Bought</Text>
+        </View>}
+        data={shoppinglist}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: StatusBar.currentHeight,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100
   },
-  map: {
-    flex: 1,
-    width: "100%",
-    height: "100%"
+  texts: {
+    marginTop: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  listcontainer: {
+    flexDirection: "row"
   }
 });
 
