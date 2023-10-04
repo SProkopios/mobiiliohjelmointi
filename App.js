c3401c62ba9e07542aaecfbedd3e3c5321dce5fc
SProkopios <s.koponen93@gmail.com>
2023-10-04 19:24:12 +0300

    Tehtävä 13

--- App.js
+++ App.js
@@ -1,91 +1,37 @@
 import { View, FlatList, TextInput, Button, StyleSheet, Text } from "react-native";
-import { initializeApp } from 'firebase/app';
-import { getDatabase, push, ref, onValue,remove, query, collection } from 'firebase/database';
-import { useState, useEffect } from "react";
-import { getApp, getApps } from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+import { useState } from "react";
+import * as Contacts from 'expo-contacts';
 
 
 export default function App() {
-  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-  const database = getDatabase(app);
-  const [amount, setAmount] = useState("");
-  const [product, setProduct] = useState("");
-  const [items, setItems] = useState([]);
-
-  // useEffect(() => {
-  //   const itemsRef = ref(database, "items/");
-  //   onValue(itemsRef, (snapshot) => {
-  //     const data = snapshot.val();
-  //     setItems(Object.values(data));
-  //   });
-  // }, []);
-
-  useEffect(() => {
-    const itemsRef = ref(database, 'items/');
-    onValue(itemsRef, (snapshot) => {
-      const data = snapshot.val();
-      const itemsArray = data ? Object.entries(data).map(([key, value]) => ({ key, ...value })) : [];
-      setItems(itemsArray);
-    });
-  }, []);
+  const [contact, setContact] = useState();
 
 
-  const saveItem = () => {
-    push(ref(database, "items/"), { product: product, amount: amount });
-    setProduct("");
-    setAmount("");
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.PhoneNumbers] }
+      );
+      if (data.length > 0) {
+        setContact(data);
+      }
+    }
   };
+    
 
-  const deleteItem = (key) => {
-    const itemRef = ref(database, `items/${key}`);
-    remove(itemRef)
-  };
+
+
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.texts}
-        placeholder="Product"
-        onChangeText={(product) => setProduct(product)}
-        value={product}
-      />
-      <TextInput
-        style={styles.texts}
-        placeholder="Amount"
-        onChangeText={(amount) => setAmount(amount)}
-        value={amount}
-      />
-      <Button onPress={saveItem} title="Save" />
-      <Text>Shopping list</Text>
       <FlatList
-        style={{ marginLeft: "5%" }}
-        keyExtractor={(item) => item.key}
-        renderItem={({ item }) => (
-          <View style={styles.listcontainer}>
-            <Text>
-              {item.product}, {item.amount}{" "}
-            </Text>
-            <Text
-              style={{ color: "#0000ff" }}
-              onPress={() => deleteItem(item.key)}
-            >
-              Bought
-            </Text>
-          </View>
-        )}
-        data={items}
+        data={ contact }
+        renderItem={({ item }) => 
+          <Text>{item.name} {item.phoneNumbers[0].number}</Text>
+        }
       />
+      <Button title="Get Contact" onPress={getContacts} />
     </View>
   );
 }
